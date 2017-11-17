e020a14bb9998d3094dce324437c413e3999025d
christophertorres0 <31553679+christophertorres0@users.noreply.github.com>
2017-11-17 14:20:47 -0800

    Create phaze.js

---
+++ phaze.js
@@ -0,0 +1,6 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+function makeOpaque(e){
+	e.classList.add("opaque");
+}
