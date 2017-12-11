48441c61034a01b0bbc0d22b8d5c89e87662ef36
christophertorres0 <31553679+christophertorres0@users.noreply.github.com>
2017-12-11 13:19:47 -0800

    Update phaze.js

--- phaze.js
+++ phaze.js
@@ -1,17 +1,21 @@
-
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play1(){
        document.getElementById("thestoneroses").play();
 	document.getElementById("thesmiths").pause();
 	document.getElementById("thecure").pause();
 }
 
-
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play2(){
        document.getElementById("thesmiths").play();
 	document.getElementById("thecure").pause()
 	 document.getElementById("thestoneroses").pause();
 }
 
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play3(){
        document.getElementById("thecure").play();
 	 document.getElementById("thestoneroses").pause();
