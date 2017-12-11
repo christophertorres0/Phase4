306dc915b553bc70743ced1f1e0137054e01ba8c
christophertorres0 <31553679+christophertorres0@users.noreply.github.com>
2017-12-11 13:14:40 -0800

    Update phaze.js

--- phaze.js
+++ phaze.js
@@ -1,9 +1,3 @@
-// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
-// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
-
-function makeOpaque(e){
-	e.classList.add("opaque");
-}
 
 function play1(){
        document.getElementById("thestoneroses").play();
